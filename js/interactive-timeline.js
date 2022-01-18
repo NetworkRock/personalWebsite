89f807f92274f51370f01de70f0d1320adc095c9
RobinLeeb <robin.leeb@yource.com>
2022-01-18 15:06:25 +0100

    Fix buggy page with not loading content

--- js/interactive-timeline.js
+++ js/interactive-timeline.js
@@ -12,16 +12,15 @@ for (let i = 0; i < flipCard.length; i++) {
   if (i === 0) {
     flipCard[i].setAttribute("style", "display:inline");
   }
-  ['click', 'touchstart'].forEach((eventType) => {
-    timelineItems[i].addEventListener(eventType, (click) => {
-      for (let card of flipCard) {
-        if (card.getAttribute("id") === click.target.getAttribute("id")) {
-          card.setAttribute("style", "display:inline");
-          card.classList.add("slide-in-fwd-center");
-        } else {
-          card.setAttribute("style", "display:none");
-        }
+
+  timelineItems[i].addEventListener('click', (click) => {
+    for (let card of flipCard) {
+      if (card.getAttribute("id") === click.target.getAttribute("id")) {
+        card.setAttribute("style", "display:inline");
+        card.classList.add("slide-in-fwd-center");
+      } else {
+        card.setAttribute("style", "display:none");
       }
-    });
-  })
+    }
+  });
 }
