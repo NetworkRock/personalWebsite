29de09012d8917499e7655fc91a3cc31fa324461
Robin <R.leeb@gmx.de>
2022-05-01 22:09:03 +0200

    test: Configure to include every js file for coverage
    - Change the test scripts in the package.json

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,9 @@ const config = {
   moduleNameMapper: {
     "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
     "\\.(css|less)$": "<rootDir>/__mocks__/fileMock.js"
-  }
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ["./src/**"],
 };
 
 module.exports = config;
